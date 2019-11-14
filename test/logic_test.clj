c9dad4878e056a79f13f8084fe724d7a230ea3e4
Andre Jasiskis <midea.andre@gmail.com>
2019-11-14 19:53:23 +0100

    initial structure

---
+++ test/logic_test.clj
@@ -0,0 +1,7 @@
+(ns allstreet-challenge.logic-test
+  (:require [clojure.test :refer :all]
+            [matcher-combinators.clj-test]
+            [matcher-combinators.matchers :as m]))
+
+(deftest "input parsing"
+  (testing "input parsing"))
