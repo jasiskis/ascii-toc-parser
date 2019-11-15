fca1d0ec489e42b9902908cf944c6431afa16bfa
Andre Jasiskis <midea.andre@gmail.com>
2019-11-15 12:39:31 +0100

    tests passing

--- src/logic.clj
+++ src/logic.clj
@@ -1,7 +1,56 @@
 (ns allstreet-challenge.logic
   (:require [clojure.string :as string]))
 
+(def last-element-in-level "└──")
+(def nested-element "├──")
+(def spacer "    ")
+(def open-parent "│")
+
 (defn parse-line [line]
   (let [[level & text] (string/split line #" ")]
     {:level (count level)
      :text (string/join " " text)}))
+
+(defn add-prefix [node]
+  (if (:last-in-level node)
+    (assoc node :prefix last-element-in-level)
+    (assoc node :prefix nested-element)))
+
+(defn add-spacer [node]
+  (->> (reduce (fn [acc open]
+                (if open
+                  (str acc open-parent spacer)
+                  (str acc spacer))) "" (:open-parents node))
+    (assoc node :spacer)))
+
+(defn build-tree [nodes]
+  (let [nodes' (reverse nodes)]
+    (loop [])))
+
+
+(defn enrich-node [node next-node]
+  (let [current-level (:level node)
+        next-node-level (:level next-node)]
+    (cond
+      (not next-node) (assoc node :last-in-level true
+                             :open-parents (take (dec current-level)
+                                                       (repeat false)))
+      (= current-level
+         next-node-level) (assoc node :last-in-level false 
+                                 :open-parents (:open-parents next-node))
+      (> current-level
+         next-node-level) (assoc node 
+                                 :last-in-level true
+                                 :open-parents 
+                                 (conj (:open-parents next-node) true)))))
+
+#_ (loop [[a & rest] [5 4 4 3 2 1]
+       previous-node {}
+       tree []]
+  (if-not previous-node
+    (recur ))
+  (cond
+    ())
+  (if-not rest
+    tree
+    (recur rest)))
--- test/logic_test.clj
+++ test/logic_test.clj
@@ -16,6 +16,94 @@
                 (logic/parse-line "## heading test test"))))
 
   (testing "N level heading"
-    (is (match? {:level 15 
+    (is (match? {:level 15
                  :text "heading test test"}
                 (logic/parse-line (str (apply str (take 15 (repeat "#"))) " heading test test"))))))
+
+(deftest add-prefix-test
+  (testing "for single level add └──"
+    (is (match? {:prefix "└──"}
+                (logic/add-prefix {:level 1
+                                   :text "heading test test"
+                                   :last-in-level true}))))
+
+  (testing "for nested level should add ├──"
+    (is (match? {:prefix "├──"}
+                (logic/add-prefix {:level 2
+                                   :text "heading 2"})))))
+
+(deftest add-spacer-test 
+  (testing "should not add space for level 1"
+    (is (match? {:spacer ""}
+                (logic/add-spacer {:level 1
+                                   :text "abc"
+                                   :open-parents []}))))
+
+  (testing "should add a spacer for level 2 wo open parents"
+    (is (match? {:spacer logic/spacer}
+                (logic/add-spacer {:level 2
+                                   :text "abc"
+                                   :open-parents [false]}))))
+
+  (testing "should add a | and a spacer for level 2 with open parent"
+    (is (match? {:spacer (str logic/open-parent logic/spacer)}
+                (logic/add-spacer {:level 2
+                                   :text "abc"
+                                   :open-parents [true]}))))
+
+  (testing "should add a |  spacer  |  spacer | spacer for level 3 with 3 open parent"
+    (is (match? {:spacer (str logic/open-parent logic/spacer logic/open-parent logic/spacer logic/open-parent logic/spacer)}
+                (logic/add-spacer {:open-parents [true true true]}))))
+
+  (testing "should add a |  spacer  spacer | spacer for level 3 with 2 open parent"
+    (is (match? {:spacer (str logic/open-parent logic/spacer logic/spacer logic/open-parent logic/spacer)}
+                (logic/add-spacer {:open-parents [true false true]})))))
+
+
+#_(deftest build-tree
+  (testing "single level tree"
+    (is (match? {:level 0
+                 :children 
+                 [{:level 1
+                  :text "abc"
+                  :children []}]}
+                (logic/build-tree [{:level 1
+                                    :text "abc"}]))))
+  (testing "two level tree"
+    (is (match? {:level 0
+                 :children
+                 [{:level 1
+                   :text "abc"
+                   :children 
+                   [{:level 2
+                     :text "abc"
+                     :open-parents [false]}]}]}
+                (logic/build-tree [{:level 1
+                                    :text "abc"}
+                                   {:level 2
+                                    :text "abc"}])))))
+(deftest enrich-node
+  (testing "single-node"
+    (is (match? {:level 1
+                 :open-parents []
+                 :last-in-level true}
+                (logic/enrich-node {:level 1} nil))))
+  (testing "two nodes same level"
+    (is (match? {:level 1
+                 :open-parents []
+                 :last-in-level false}
+                (logic/enrich-node {:level 1} {:level 1
+                                               :last-in-level true
+                                               :open-parents []}))))
+  (testing "last-node should have level - 1 closed parents"
+    (is (match? {:level 5
+                 :open-parents [false false false false]
+                 :last-in-level true}
+                (logic/enrich-node {:level 5} nil))))
+  (testing "add open parent for current level greater than next"
+    (is (match? {:level 5
+                 :open-parents [true false true true]
+                 :last-in-level true}
+                (logic/enrich-node {:level 5} {:level 4
+                                               :last-in-level true
+                                               :open-parents [true false true]})))))
